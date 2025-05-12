5b08811a2771028c0cf083296abf6d0f5e2e6359
Viktor Lyhogodenko <devvasylyna@gmail.com>
2025-05-12 23:17:15 +0300

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,9 @@
-import express, { Request, Response, NextFunction } from "express";
+import app from "./app";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 4000;
-
-app.use(express.json());
-
-app.use("/api", userRoutes);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({ message: "Resource not found" });
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({ message: "Internal Server Error" });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
